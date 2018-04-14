5c4e6c6bbb8ec56a5fea4b89d333478783f495ec
er713 <33582219+er713@users.noreply.github.com>
2018-04-14 03:11:16 +0200

    Add files via upload

--- Zadanko.py
+++ Zadanko.py
@@ -1,5 +1,6 @@
 from itertools import product, cycle
 from random import randrange
+from math import floor
 
 
 def main():
@@ -15,14 +16,49 @@ def main():
     border = (2 * R + 1) * step
     print(border)
     atoms = []
+    ns = [0*i for i in range(len(states))]
     atomsleft = N
     for i in cycle(range(0, border, step)):
         atoms.append(randrange(i, i + step))
+        ns[atoms[len(atoms)-1]] = ns[atoms[len(atoms)-1]]+1
         atomsleft -= 1
         if atomsleft == 0:
             break
     for i in range(N):
         print(states[atoms[i]])
+        # print(ns[atoms[i]])
+    print(P)
+    print(states[atoms[1]])
+    atoms[1] += 5*border + 3*step
+    print(states[atoms[1]])
+
+    #czas
+    j = int(input())
+    tj = j*deltat
+    for i in range(len(atoms)):
+        xatoms = floor(states[atoms[i]][0] + tj*states[atoms[i]][2])
+        yatoms = floor(states[atoms[i]][1] + tj*states[atoms[i]][3])
+        ns[atoms[i]] = ns[atoms[i]] - 1
+        while xatoms > R or xatoms < -R:
+            print("a")
+            if xatoms > R:
+                xatoms = R - (xatoms - abs(R - states[atoms[i]][0])) # odbicie od ściany
+                atoms[i] -= (2 * P + 1) * (2 * abs(states[atoms[i]][2])) # mnożenie wektora razy -1
+            elif xatoms < R:
+                xatoms = -R + xatoms - abs(-R - states[atoms[i]][0]) # odbicie od ściany
+                atoms[i] += (2 * P + 1) * (2 * abs(states[atoms[i]][2])) # mnożenie wektora razy -1
+        while yatoms > R or yatoms < -R:
+            print("b")
+            if yatoms > R:
+                yatoms = R - (yatoms - abs(R - states[atoms[i]][1])) # odbicie od ściany
+                atoms[i] -= 2*abs(states[atoms[i]][3]) # mnożenie wektora razy -1
+            elif yatoms < -R:
+                yatoms = -R + yatoms - abs(-R - states[atoms[i]][1]) # odbicie od ściany
+                atoms[i] += 2*abs(states[atoms[i]][3]) # mnożenie wektora razy -1
+        atoms[i] += (xatoms - states[atoms[i]][0])*border + (yatoms - states[atoms[i]][1])*step # zamiana miejsca atomu w tuple
+        ns[atoms[i]] = ns[atoms[i]] + 1
+
+
 
 
 
